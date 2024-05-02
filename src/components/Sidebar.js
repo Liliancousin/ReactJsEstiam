cacb3863b4899602fa35dba73cbe70bd20fd81ec
Liliancousin <lilian.cousin@estiam.com>
2024-05-02 09:33:59 +0200

    maj2

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,8 +3,6 @@ import LinkButton from "./LinkButton";
 const Sidebar = () => {
   return (
     <div className="dark sidebar">
-      <div>Nom prenom</div>
-
       <LinkButton>Evenements</LinkButton>
       <LinkButton>Joueurs</LinkButton>
       <LinkButton>Equipes</LinkButton>
